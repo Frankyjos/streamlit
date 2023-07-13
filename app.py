5224eb1fe637559599d31f50eea6d761a26cf6a7
Franky Rivero <franky.rivero@newtoms.com>
2023-07-13 10:05:45 -0400

    change banner

--- app.py
+++ app.py
@@ -2,33 +2,22 @@ import streamlit as st
 
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
-page_bg_img = """
-<style>
-[class="css-10trblm"] {
-    background-color=#e5e5f7
-}
-
-</style>
-"""
 
 # ------ HEADER SECTION -------------
     
 with st.sidebar: 
-    st.image("https://newtomstestbucket2.s3.amazonaws.com/FONDO_AZUL_LETRAS__AZUL_CLARO-removebg-preview.png")
-    st. header("GenAI Content Analyzer")
+    st.image("logonewtoms_remove.png")
     st.selectbox('Select the count of auto-prompts to consider', ['1', '2','3'])
 
     st. header("Generated auto-prompts")
-    st.button("What type of furniture is in the room?")
-    st.button("How does the room's decor reflect the homeowner's personality")
+    st.write("1. What type of furniture is in the room?")
+    st.write("2. How does the room's decor reflect the homeowner's personality")
 
 with st.container():
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    st.image("https://newtomstestbucket2.s3.amazonaws.com/Screenshot+2023-07-10+212505.jpg")
-    st.write("---")
-    
+    st.image("GenAI Contect Analyzer (4).png")
+
 with st.container():
-    st.write ("Instructions:")
+    st.subheader ("Instructions:")
     st.write(
         """
         1. Browse ans select an input file in one of the supported formats.
